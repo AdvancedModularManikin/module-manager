895eb10deea0c647a66971bbee9e24800caf95ef
Doug Raum <dougr@vcom3d.com>
2022-03-26 23:04:49 -0400

    Add scenario loading / configuration publishing

--- src/ModuleManager.h
+++ src/ModuleManager.h
@@ -13,112 +13,128 @@
 namespace AMM {
 
 /// Definition for Logs.
-struct LogEntry {
-   std::string source;
-   std::string topic;
-   std::string event_id;
-   uint64_t timestamp;
-   std::string data = "";
-};
+    struct LogEntry {
+        std::string source;
+        std::string topic;
+        std::string event_id;
+        uint64_t timestamp;
+        std::string data = "";
+    };
 
 /// Container for Module Manager logic.
-class ModuleManager : ListenerInterface {
+    class ModuleManager : ListenerInterface {
 
-public:
+    public:
 
 
-private:
+    private:
 
-protected:
+    protected:
 
-   /// This module's id.
-   AMM::UUID m_uuid;
+        /// This module's id.
+        AMM::UUID m_uuid;
 
-   /// This module's name.
-   std::string moduleName = "AMM_ModuleManager";
+        /// This module's name.
+        std::string moduleName = "AMM_ModuleManager";
 
-   /// This module's path to the config file.
-   const std::string config_file = "config/module_manager_amm.xml";
+        /// This module's path to the config file.
+        const std::string config_file = "config/module_manager_amm.xml";
 
-   /// DDS Manager for this module.
-   DDSManager<ModuleManager> *m_mgr = new DDSManager<ModuleManager>(config_file);
+        /// DDS Manager for this module.
+        DDSManager <ModuleManager> *m_mgr = new DDSManager<ModuleManager>(config_file);
 
-   std::mutex m_mapmutex;
+        std::mutex m_mapmutex;
 
-public:
-   ModuleManager();
+    public:
+        ModuleManager();
 
-   ~ModuleManager();
+        ~ModuleManager();
 
-   void PublishOperationalDescription();
+        void PublishOperationalDescription();
 
-   void PublishConfiguration();
+        void PublishConfiguration();
 
-   void Shutdown();
+        void Shutdown();
 
-   void ShowStatus();
+        void ShowStatus();
 
-   void WriteLogEntry(LogEntry log);
+        void WriteLogEntry(LogEntry log);
 
-   void ClearEventLog();
+        void ParseScenarioFromFile(const std::string xmlFileName);
 
-   void ClearDiagnosticLog();
+        void ClearEventLog();
 
-   std::string ExtractGUIDToString(GUID_t guid);
-   uint64_t GetTimestamp();
+        void ClearDiagnosticLog();
 
-private:
-   /// Enables
-   void RunSimulation();
+        std::string ExtractGUIDToString(GUID_t guid);
 
-   void HaltSimulation();
+        uint64_t GetTimestamp();
 
-   void ResetSimulation();
+    private:
+        /// Enables
+        void RunSimulation();
 
-   void SaveSimulation();
+        void HaltSimulation();
 
-protected:
+        void ResetSimulation();
 
-   /// Event listener for Logs.
-   void onNewLog(AMM::Log& log, SampleInfo_t *info);
+        void SaveSimulation();
 
-   /// Event listener for Module Configuration
-   void onNewModuleConfiguration(AMM::ModuleConfiguration& mc, SampleInfo_t *info);
+        const std::string loadScenarioPrefix = "LOAD_SCENARIO:";
+        const std::string loadStatePrefix = "LOAD_STATE:";
+        const std::string sysPrefix = "[SYS]";
+        const std::string actPrefix = "[ACT]";
+        const std::string loadPrefix = "LOAD_STATE:";
 
-   /// Event listener for Status
-   void onNewStatus(AMM::Status &status, SampleInfo_t *info);
+        std::string currentScenario = "NONE";
+        std::string currentState = "NONE";
+        std::string currentStatus = "NOT RUNNING";
 
-   /// Event listener for Simulation Controller
-   void onNewSimulationControl(AMM::SimulationControl &simControl, SampleInfo_t *info);
+        bool isPaused = false;
 
-   /// Event listener for Assessment.
-   void onNewAssessment(AMM::Assessment &assessment, SampleInfo_t *info);
+    protected:
 
-   /// Event listener for Event Fragment.
-   void onNewEventFragment(AMM::EventFragment &eventFrag, SampleInfo_t *info);
+        /// Event listener for Logs.
+        void onNewLog(AMM::Log &log, SampleInfo_t *info);
 
-   /// Event listener for Event Record.
-   void onNewEventRecord(AMM::EventRecord &eventRec, SampleInfo_t *info);
+        /// Event listener for Module Configuration
+        void onNewModuleConfiguration(AMM::ModuleConfiguration &mc, SampleInfo_t *info);
 
-   /// Event listener for Fragment Amendment Request.
-   void onNewFragmentAmendmentRequest(AMM::FragmentAmendmentRequest &ffar, SampleInfo_t *info);
+        /// Event listener for Status
+        void onNewStatus(AMM::Status &status, SampleInfo_t *info);
 
-   /// Event listener for Omitted Event.
-   void onNewOmittedEvent(AMM::OmittedEvent &omittedEvent, SampleInfo_t *info);
+        /// Event listener for Simulation Controller
+        void onNewSimulationControl(AMM::SimulationControl &simControl, SampleInfo_t *info);
 
-   /// Event listener for Operational Description.
-   void onNewOperationalDescription(AMM::OperationalDescription &opDescript, SampleInfo_t *info);
+        /// Event listener for Assessment.
+        void onNewAssessment(AMM::Assessment &assessment, SampleInfo_t *info);
 
-   /// Event listener for Render Modification.
-   void onNewRenderModification(AMM::RenderModification &rendMod, SampleInfo_t *info);
+        /// Event listener for Event Fragment.
+        void onNewEventFragment(AMM::EventFragment &eventFrag, SampleInfo_t *info);
 
-   /// Event listener for Physiology Modification.
-   void onNewPhysiologyModification(AMM::PhysiologyModification &physMod, SampleInfo_t *info);
+        /// Event listener for Event Record.
+        void onNewEventRecord(AMM::EventRecord &eventRec, SampleInfo_t *info);
 
-   /// Event lsitener for Command.
-   void onNewCommand(AMM::Command &command, eprosima::fastrtps::SampleInfo_t *info);
+        /// Event listener for Fragment Amendment Request.
+        void onNewFragmentAmendmentRequest(AMM::FragmentAmendmentRequest &ffar, SampleInfo_t *info);
 
-};
+        /// Event listener for Omitted Event.
+        void onNewOmittedEvent(AMM::OmittedEvent &omittedEvent, SampleInfo_t *info);
+
+        /// Event listener for Operational Description.
+        void onNewOperationalDescription(AMM::OperationalDescription &opDescript, SampleInfo_t *info);
+
+        /// Event listener for Render Modification.
+        void onNewRenderModification(AMM::RenderModification &rendMod, SampleInfo_t *info);
+
+        /// Event listener for Physiology Modification.
+        void onNewPhysiologyModification(AMM::PhysiologyModification &physMod, SampleInfo_t *info);
+
+        /// Event lsitener for Command.
+        void onNewCommand(AMM::Command &command, eprosima::fastrtps::SampleInfo_t *info);
+
+        void ParseCapabilities(tinyxml2::XMLElement *node);
+    };
 
 
 } // namespace AMM
--- src/ModuleManagerMain.cpp
+++ src/ModuleManagerMain.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 using namespace sqlite;
 
+
 bool closed = false;
 int daemonize = 0;
 bool setup = false;
@@ -15,182 +16,182 @@ bool wipe = false;
 
 /// Clears database tables.
 void WipeTables() {
-   try {
-      sqlite_config config;
-      database db("amm.db", config);
+    try {
+        sqlite_config config;
+        database db("amm.db", config);
 
-      db << "delete from events;";
-      db << "delete from module_capabilities;";
-      db << "delete from module_status;";
-      db << "delete from logs;";
+        db << "delete from events;";
+        db << "delete from module_capabilities;";
+        db << "delete from module_status;";
+        db << "delete from logs;";
 
-   } catch (exception &e) {
-      LOG_ERROR << e.what();
-   }
+    } catch (exception &e) {
+        LOG_ERROR << e.what();
+    }
 
 }
 
 /// On start initialization for database tables.
 void SetupTables() {
 
-   try {
-      sqlite_config config;
-      database db("amm.db", config);
+    try {
+        sqlite_config config;
+        database db("amm.db", config);
 
-      LOG_INFO << "Creating event log table...";
-      db << "create table if not exists events("
-            "source text,"
-            "module_id text,"
-            "module_guid text,"
-            "module_name text,"
-            "event_id text,"
-            "topic text,"
-            "timestamp bigint,"
-            "data text"
-            ");";
+        LOG_INFO << "Creating event log table...";
+        db << "create table if not exists events("
+              "source text,"
+              "module_id text,"
+              "module_guid text,"
+              "module_name text,"
+              "event_id text,"
+              "topic text,"
+              "timestamp bigint,"
+              "data text"
+              ");";
 
-      LOG_INFO << "Creating module capabilities table...";
-      db << "create table if not exists module_capabilities ("
-            "module_id text,"
-            "module_guid text,"
-            "module_name text,"
-            "description text,"
-            "manufacturer text,"
-            "model text,"
-            "module_version text,"
-            "serial_number text,"
-            "capabilities text"
-            ");";
+        LOG_INFO << "Creating module capabilities table...";
+        db << "create table if not exists module_capabilities ("
+              "module_id text,"
+              "module_guid text,"
+              "module_name text,"
+              "description text,"
+              "manufacturer text,"
+              "model text,"
+              "module_version text,"
+              "serial_number text,"
+              "capabilities text"
+              ");";
 
-      LOG_INFO << "Creating module status table...";
-      db << "create table if not exists module_status ("
-            "module_id text,"
-            "module_guid text,"
-            "module_name text,"
-            "capability text,"
-            "status text,"
-            "message text,"
-            "timestamp bigint,"
-            "encounter_id text"
-            ");";
+        LOG_INFO << "Creating module status table...";
+        db << "create table if not exists module_status ("
+              "module_id text,"
+              "module_guid text,"
+              "module_name text,"
+              "capability text,"
+              "status text,"
+              "message text,"
+              "timestamp bigint,"
+              "encounter_id text"
+              ");";
 
-      LOG_INFO << "Creating log record table...";
-      db << "create table if not exists logs("
-            "module_id text,"
-            "module_guid text,"
-            "module_name text,"
-            "message text,"
-            "log_level text,"
-            "timestamp bigint"
-            ");";
+        LOG_INFO << "Creating log record table...";
+        db << "create table if not exists logs("
+              "module_id text,"
+              "module_guid text,"
+              "module_name text,"
+              "message text,"
+              "log_level text,"
+              "timestamp bigint"
+              ");";
 
-   } catch (exception &e) {
-      LOG_ERROR << e.what();
-   }
+    } catch (exception &e) {
+        LOG_ERROR << e.what();
+    }
 
-   WipeTables();
+    WipeTables();
 }
 
 /// Displays current manager configuration.
 static void ShowUsage(const std::string &name) {
-   cerr << "Usage: " << name << " <option(s)>"
-        << "\nOptions:\n"
-        << "\t-a\t\t\tAuto start\n"
-        << "\t-d\t\t\tDaemonize\n"
-        << "\t-s\t\t\tSetup module manager tables\n"
-        << "\t-w\t\t\tWipe tables\n"
-        << "\t-h,--help\t\t\tShow this help message\n"
-        << endl;
+    cerr << "Usage: " << name << " <option(s)>"
+         << "\nOptions:\n"
+         << "\t-a\t\t\tAuto start\n"
+         << "\t-d\t\t\tDaemonize\n"
+         << "\t-s\t\t\tSetup module manager tables\n"
+         << "\t-w\t\t\tWipe tables\n"
+         << "\t-h,--help\t\t\tShow this help message\n"
+         << endl;
 }
 
 /// Main menu for Module Manager.
 void ShowMenu(AMM::ModuleManager *modManager) {
-   string action;
+    string action;
 
-   cout << endl;
-   cout << " [1]Status " << endl;
-   cout << " [2]Setup tables" << endl;
-   cout << " [3]Wipe tables" << endl;
-   cout << " [4]Shutdown" << endl;
-   cout << " >> ";
+    cout << endl;
+    cout << " [1]Status " << endl;
+    cout << " [2]Setup tables" << endl;
+    cout << " [3]Wipe tables" << endl;
+    cout << " [4]Shutdown" << endl;
+    cout << " >> ";
 
-   getline(cin, action);
+    getline(cin, action);
 
-   transform(action.begin(), action.end(), action.begin(), ::toupper);
+    transform(action.begin(), action.end(), action.begin(), ::toupper);
 
-   if (action == "1") {
-      modManager->ShowStatus();
-   } else if (action == "2") {
-      SetupTables();
-   } else if (action == "3") {
-      WipeTables();
-   } else if (action == "4") {
-      LOG_INFO << "Shutting down Module Manager.";
-      closed = true;
-      modManager->Shutdown();
-   } else {
-      /// TODO: Unknown menu action.
-   }
+    if (action == "1") {
+        modManager->ShowStatus();
+    } else if (action == "2") {
+        SetupTables();
+    } else if (action == "3") {
+        WipeTables();
+    } else if (action == "4") {
+        LOG_INFO << "Shutting down Module Manager.";
+        closed = true;
+        modManager->Shutdown();
+    } else {
+        /// TODO: Unknown menu action.
+    }
 }
 
 /// Main program
 int main(int argc, char *argv[]) {
-   static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
-   plog::init(plog::verbose, &consoleAppender);
+    static plog::ColorConsoleAppender <plog::TxtFormatter> consoleAppender;
+    plog::init(plog::verbose, &consoleAppender);
 
-   LOG_INFO << "AMM - Module Manager";
+    LOG_INFO << "AMM - Module Manager";
 
-   for (int i = 1; i < argc; ++i) {
-      string arg = argv[i];
+    for (int i = 1; i < argc; ++i) {
+        string arg = argv[i];
 
-      if ((arg == "-h") || (arg == "--help")) {
-         ShowUsage(argv[0]);
-         return 0;
-      }
+        if ((arg == "-h") || (arg == "--help")) {
+            ShowUsage(argv[0]);
+            return 0;
+        }
 
-      if (arg == "-d") {
-         daemonize = 1;
-      }
+        if (arg == "-d") {
+            daemonize = 1;
+        }
 
-      if (arg == "-a") {
-         autostart = 1;
-      }
+        if (arg == "-a") {
+            autostart = 1;
+        }
 
-      if (arg == "-s") {
-         setup = true;
-      }
+        if (arg == "-s") {
+            setup = true;
+        }
 
-      if (arg == "-w") {
-         wipe = true;
-      }
-   }
+        if (arg == "-w") {
+            wipe = true;
+        }
+    }
 
-   if (setup) {
-      LOG_INFO << "Creating AMM database schema.";
-      SetupTables();
-   }
+    if (setup) {
+        LOG_INFO << "Creating AMM database schema.";
+        SetupTables();
+    }
 
-   if (wipe) {
-      LOG_INFO << "Wiping tables on startup";
-      WipeTables();
-   }
+    if (wipe) {
+        LOG_INFO << "Wiping tables on startup";
+        WipeTables();
+    }
 
-   AMM::ModuleManager modManager;
+    AMM::ModuleManager modManager;
 
-   std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
 
-   modManager.PublishOperationalDescription();
-   modManager.PublishConfiguration();
+    modManager.PublishOperationalDescription();
+    modManager.PublishConfiguration();
 
-   while (!closed) {
-      if (autostart != 1) {
-         ShowMenu(&modManager);
-      }
-      std::this_thread::sleep_for(std::chrono::milliseconds(250));
-   }
+    while (!closed) {
+        if (autostart != 1) {
+            ShowMenu(&modManager);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    }
 
-   modManager.Shutdown();
-   LOG_INFO << "Exiting.";
+    modManager.Shutdown();
+    LOG_INFO << "Exiting.";
 
-   return 0;
+    return 0;
 }
